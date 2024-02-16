8c2d6912c8c91852fa5df523cd8769ad6bd931d4
Hithaishi's hub <116420172+hithaishi-25@users.noreply.github.com>
2024-02-16 14:20:43 +0000

    variables are learnt

---
+++ 01_basics/var.js
@@ -0,0 +1,11 @@
+const accountId=38400
+let accountName="hitha"
+var accountPwd="hi568"
+city="hyderabad"
+
+
+accountName="shana"
+accountPwd="647328"
+city="kerala"
+
+console.table([accountId, accountName, accountPwd, city])
